0a08ce62fb06b6811b54780028c3309e346edba6
sallaumen <lc_tavano@hotmail.com>
2020-11-08 13:55:43 -0300

    Finishing Chapter7

--- lib/chapter7/chapter7.ex
+++ lib/chapter7/chapter7.ex
@@ -1,5 +1,40 @@
 defmodule Chapter7 do
 
-  # Exercise: -1
+  # Exercise: ListsAndRecursion-1
+  def mapsum(list, func) do
+    {_, result} = Enum.map(list, func)
+    |> Enum.map_reduce(0, fn (elem, acc) -> {elem, acc + elem} end)
+    result
+  end
+  # OR
+  def _mapsum(list, func) do
+    Enum.map(list, func)
+    |> Enum.sum
+  end
+
+  # Exercise: ListsAndRecursion-2
+  # Also, there is Enum.max/1
+  def max(list) do
+    list |> Enum.map_reduce(0, fn (tuple, max) -> {tuple, get_max_from_2_values(tuple, max)} end)
+  end
+
+  defp get_max_from_2_values(value1, value2) when value1 > value2, do: value1
+  defp get_max_from_2_values(_, value2), do: value2
+
+  # Exercise: ListsAndRecursion-3
+  def caesar(list, n) do
+    list |> Enum.map(&(wrap_if_over_122(&1 + n)))
+  end
+
+  defp wrap_if_over_122(value) when value <= 122, do: value
+  defp wrap_if_over_122(_), do: 63
+
+  # Exercise: ListsAndRecursion-3
+  def spam(from, to) do
+    gen_element_list(from, to, from)
+  end
+
+  defp gen_element_list(from, to, actual) when actual == to, do: [actual]
+  defp gen_element_list(from, to, actual) when actual < to, do: [actual | gen_element_list(from, to, actual + 1)]
 
 end
